2760c1f22a1ecb6d6e27326fd50ae4ade186e03a
GeoDaz <gfrydz2@gmail.com>
2020-02-05 19:55:21 +0100

    logout

--- feedback-ecole-ipssi/src/components/pages/Logout.js
+++ feedback-ecole-ipssi/src/components/pages/Logout.js
@@ -1,17 +1,28 @@
+// modules
 import React, { Component } from "react";
-import { withRouter } from "react-router";
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+// actions
+import {removeUser} from '../../reducer';
+// const
+import { STORED_USER } from "../../constants";
 
 class Logout extends Component {
 
-    // TODO logout functions
+    UNSAFE_componentWillMount() {
+        this.props.removeUser();
+        localStorage.removeItem(STORED_USER);
+        this.props.history.push('/login');
+    }
 
     render() {
         return (
-            <div className="logout">
+            <main className="logout">
+                <h1>Déconnexion</h1>
                 {/* TODO : make a loading circle */}
-            </div>
+            </main>
         );
     }
 }
 
-export default withRouter(Logout);
+export default withRouter(connect(null, {removeUser})(Logout));
