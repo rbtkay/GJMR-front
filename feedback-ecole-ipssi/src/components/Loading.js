0f2e436347eed4c70c95b76d7076df3851d33665
GeoDaz <gfrydz2@gmail.com>
2020-02-06 18:56:23 +0100

    Add loading component

---
+++ feedback-ecole-ipssi/src/components/Loading.js
@@ -0,0 +1,20 @@
+
+//module
+import React from 'react';
+
+function Loading() {
+    return (
+        <div className="loading">
+            <div>
+            </div>
+            <div>
+            </div>
+            <div>
+            </div>
+            <div>
+            </div>
+        </div>
+    );
+}
+
+export default Loading;
