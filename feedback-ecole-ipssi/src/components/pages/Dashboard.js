feb1bf88f8c09627866a080ce8841b50098279a5
GeoDaz <gfrydz2@gmail.com>
2020-02-07 22:45:32 +0100

    get modules notes average

--- feedback-ecole-ipssi/src/components/pages/Dashboard.js
+++ feedback-ecole-ipssi/src/components/pages/Dashboard.js
@@ -43,20 +43,51 @@ class Dashboard extends Component {
 
     async getTeachersModules(modules) {
         let teachers_id = modules.map(module => module.teacher_id);
-        teachers_id = teachers_id.filter((id, i) => teachers_id.indexOf(id) === i); // distinct
+        teachers_id = teachers_id.filter(
+            (id, i) => teachers_id.indexOf(id) === i
+        ); // distinct
         const response = await request(`/users`, this.props.user.token, {
             method: "POST",
             body: teachers_id
         });
         console.log(response);
         if (this.responseManagment(response)) {
+            if (response.result.length) {
+                modules = modules.map(module => {
+                    module.teacher = response.result.filter(
+                        teacher => teacher._id === module.teacher_id
+                    )[0];
+                    return module;
+                });
+            }
+            this.getNotesModules(modules);
+            this.setState({ modules });
+        }
+    }
+
+    async getNotesModules(modules) {
+        let modules_id = modules.map(module => module._id);
+        console.log(modules_id);
+        const response = await request(
+            `/notes/modules`,
+            this.props.user.token,
+            {
+                method: "POST",
+                body: modules_id
+            }
+        );
+        console.log(response);
+        if (this.responseManagment(response) && response.result.length) {
             modules = modules.map(module => {
-                module.teacher = response.result.filter(
-                    teacher => teacher._id === module.teacher_id
-                )[0];
+                module.notes = response.result.filter(
+                    note => note.module_id === module._id
+                );
+                module.average =
+                    module.notes.reduce((sum, note) => sum + note.value, 0) 
+                    / module.notes.length;
+                this.setState({ modules });
                 return module;
             });
-            this.setState({ modules });
         }
     }
 
