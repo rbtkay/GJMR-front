cc3f8d68bc9e513a2d27972b30d0db10fca13c63
rbtkay <robert.g.khayat@gmail.com>
2020-02-08 16:31:00 +0100

    admin can create a new module, it is automatically added to the year selected

--- feedback-ecole-ipssi/src/components/pages/AddUser.js
+++ feedback-ecole-ipssi/src/components/pages/AddUser.js
@@ -5,12 +5,13 @@ import Form from '../form/Form'
 import { request } from "../../functions/fetch";
 import { STORED_USER } from "../../constants";
 import { setUser, setLog } from '../../reducer/actions';
+import Loading from '../Loading';
 
 class AddUser extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            select_options: null
+            isLoading: true
         }
 
         this.addUser = this.addUser.bind(this);
@@ -20,74 +21,79 @@ class AddUser extends Component {
         if (!this.props.user) {
             this.props.history.push(`/${this.props.user.role}/dashboard`);
         }
+
+        if (window.location.href.split('-')[1] != "student") {
+            this.setState({ isLoading: false, select_options: options });
+            return;
+        }; //we don't need the schoolyears in case it is not a student 
+
+        let response = await request(`/school-year`, { method: "GET" });
+        let options = response.result.map(promo => {
+            return promo
+        })
+        this.setState({ isLoading: false, select_school_year: options });
     }
 
     render() {
         let title = window.location.href.split('-')[1] == 'student' ? "etudiant" : "intervenant";
-        return (
-            <main>
-                <h1>Nouvel {title}</h1>
-                <Form
-                    form_items={[
-                        {
-                            type: "text",
-                            name: "last_name",
-                            label: "Nom",
-                            required: true
-                        },
-                        {
-                            type: "text",
-                            name: "first_name",
-                            label: "Prenom",
-                            required: true
-                        },
-                        {
-                            type: "text",
-                            name: "email",
-                            label: "Email",
-                            required: true
-                        },
-                    ]}
-                    select_options={this.state.select_options}
-                    callback={this.addUser}
-                />
+        if (this.state.isLoading) {
+            return (<Loading />)
+        } else {
+            return (
+                <main>
+                    <h1>Nouvel {title}</h1>
+                    <Form
+                        form_items={[
+                            {
+                                type: "text",
+                                name: "last_name",
+                                label: "Nom",
+                                required: true
+                            },
+                            {
+                                type: "text",
+                                name: "first_name",
+                                label: "Prenom",
+                                required: true
+                            },
+                            {
+                                type: "text",
+                                name: "email",
+                                label: "Email",
+                                required: true
+                            },
+                        ]}
+                        select_role={window.location.href.split('-')[1]}
+                        select_school_year={this.state.select_school_year}
+                        callback={this.addUser}
+                    />
 
-            </main >
-        )
+                </main >
+            )
+        }
     }
 
     async addUser(form_result) {
         let user_role = window.location.href.split('-')[1]; //used to know what kind of user to add
         if (user_role != "teacher" && user_role != "student") this.props.history.push(`/login`)
 
-        console.log(user_role)
         let new_user = {
             email: form_result['email'],
             last_name: form_result['last_name'],
             first_name: form_result['first_name'],
             role: user_role,
-            school_year: form_result["select_options"]
+            school_year: form_result["select_school_year"]
         }
+        console.log("new user", new_user);
         //TODO: add token
         let response = await request(`/user`, { method: "POST", body: new_user });
-        if (response.status === 201) {
+        console.log(response);
+        if (response.status === 201 || response.status === 200) {
             console.log("user inserted");
             this.props.history.push(`/${this.props.user.role}/dashboard`);
         }
         localStorage.getItem(STORED_USER);
     }
-
-    async componentWillMount() {
-        if (window.location.href.split('-')[1] != "student") return; //we don't need the schoolyears in case it is not a student 
-
-        let response = await request(`/school-year`, { method: "GET" });
-        console.log('response', response)
-        let options = response.map(promo => {
-            return promo["name"]
-        })
-        this.setState({ select_options: response });
-    }
-
 }
 
 const mapStateToProps = state => {
